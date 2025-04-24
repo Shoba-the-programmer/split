c71420fa0b9be914a3d964089065006489ee947e
Todun Shonekan <todu.shonekan@gmail.com>
2025-04-24 01:28:16 +0100

    db <p>  test commit complete

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const {Client} = require('pg');
+let un = null;
 const client = new Client(
     {
         user:"postgres",
@@ -10,8 +11,15 @@ const client = new Client(
 )
 client.connect().then(function(){
     console.log("Database Connected Successfully!")
+    client.query("SELECT username FROM split LIMIT 1", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        un = result;
+      });
 }).catch(function(err){
     console.log(err,"Failed to connect to Database")
 })
 
 module.exports = client;
+
+let ownquery ="SELECT username FROM split LIMIT 1"
