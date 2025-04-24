237a9aabb871804298fdbba29546b63cf38f88b8
Todun Shonekan <todu.shonekan@gmail.com>
2025-04-24 01:55:44 +0100

    db <p>  test commit complete

--- db.js
+++ db.js
@@ -13,10 +13,11 @@ client.connect().then(function(){
     console.log("Database Connected Successfully!")
     client.query("SELECT username FROM users LIMIT 1", function (err, result, fields) {
         if (err) throw err;
-        console.log(result);
+        //console.log(result);
         //okay okay so the result inaan object, the username is 0
         
         const username = result[0].username;
+        console.log(username);
                 app.get('/', (req, res) => {
                     res.send(username);
                 });
