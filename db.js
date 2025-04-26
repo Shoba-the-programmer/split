b68c70ca79e9ba0c63678c3b152be556ed270da9
Todun Shonekan <todu.shonekan@gmail.com>
2025-04-26 01:48:46 +0100

    db console log commit complete

--- db.js
+++ db.js
@@ -1,32 +1,39 @@
-const {Client} = require('pg');
+const { Client } = require('pg');
 let un = null;
 const client = new Client(
     {
-        user:"postgres",
-        host:"localhost",
-        database:"split",
-        port:"5432",
-        password:"root"
+        user: "postgres",
+        host: "localhost",
+        database: "split",
+        port: "5432",
+        password: "root"
     }
 )
-client.connect().then(function(){
+client.connect().then(function () {
     console.log("Database Connected Successfully!")
-    client.query("SELECT username FROM users LIMIT 1", function (err, result, fields) {
+
+    client.query("SELECT username FROM users LIMIT 1", function (err, result) {
         if (err) throw err;
         //console.log(result);
         //okay okay so the result inaan object, the username is 0
-        
-        const username = result[0].username;
-        console.log(username);
-                app.get('/', (req, res) => {
-                    res.send(username);
-                });
 
-      });
-}).catch(function(err){
-    console.log(err,"Failed to connect to Database")
+
+        if (result.length > 0) {
+            const username = result[0].username;
+            console.log(username);
+
+            app.get('/', (req, res) => {
+                res.send(username);
+            });
+        } else {
+            console.log("No users found.");
+        }
+
+    });
+}).catch(function (err) {
+    console.log(err, "Failed to connect to Database")
 })
 
 module.exports = client;
 
-let ownquery ="SELECT username FROM split LIMIT 1"
+//let ownquery ="SELECT username FROM split LIMIT 1"
--- splitSiteFiles/splitnavs.js
+++ splitSiteFiles/splitnavs.js
@@ -7,3 +7,36 @@ var chapterWritingUrl = "bookchapterpage.html";
 function gotoWebpage(page){
     window.location.href = page;
 }
+
+function trigger_CA_overlay(){
+    //calls or removes the ca overlay
+    //let view = document.getElementById('create_account_overlay').style.display;
+    var ca_overlay = document.getElementById('create_account_overlay');
+    if (ca_overlay.style.display === 'none' || ca_overlay.style.display ===""){
+        ca_overlay.style.display = 'flex';
+    } else {
+        ca_overlay.style.display = 'none';
+    }
+}
+
+function is_empty(thing){
+    if(thing.value.trim() === null || thing.value.trim() === ""){
+        return true;
+    }
+}
+
+function checklogininfo(un,pass){
+    //compare the login information to the data stored in the database
+}
+
+function check_login_entry(){
+    let un = document.getElementById('login_username');
+    let pass = document.getElementById('login_password');
+    var badDataAlert = document.getElementById('alert');
+
+    if (is_empty(un) == true || is_empty(pass) == true){
+        //if either is empty then alert that it's empty
+        badDataAlert.innerText = "You haven't entered a username and password!";
+    }
+
+}
