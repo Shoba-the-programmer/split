1cb73cd5d01eb4a50e25a10fa685c7740ae810b4
Todun Shonekan <todu.shonekan@gmail.com>
2025-05-16 15:47:01 +0100

    user route file changed to ES

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,3 +1,4 @@
+/*
 const {Router} = require("express");
 const {createUser, getUsers, checklogins} = require("../controllers/user.controller.js")
 const api = Router()
@@ -9,3 +10,16 @@ api.get("/user",checklogins)
 
 
 module.exports = api;
+
+*/
+// routes/user.route.js
+import { Router } from "express";
+import { createUser, getUsers, checklogins } from "../controllers/user.controller.js";
+
+const api = Router();
+
+api.post("/user", createUser);
+api.get("/user", getUsers);
+api.get("/user", checklogins);  // Note: Consider the route ordering if using same route + method
+
+export default api;
